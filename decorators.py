5661907154813a518d6532196b7f71dbd2aa5f4d
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-11-10 04:10:49 -0600

    Adding rough and adding decorators

---
+++ decorators.py
@@ -0,0 +1,28 @@
+def double(myfunc):
+	def double_inside():
+		return myfunc()*2
+	return double_inside
+
+def alternate_double(myfunc):
+	return myfunc()*2
+
+def oddnumber():
+	return 3
+
+#This is returning a function
+evennumber = double(oddnumber)
+print evennumber()
+
+# This is not returning any function but just value. 
+alternate_even = alternate_double(oddnumber)
+print  alternate_even
+
+#to alter the existing function value we use
+oddnumber = double(oddnumber)
+print oddnumber()
+
+#the above alteration can be done by using decorators
+@double
+def anotherodd():
+	return 7
+print anotherodd()
