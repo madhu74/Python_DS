179c0f2e3bc4e5e90a4ca998f94dcbf5a4fb1f20
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-11-22 23:20:51 -0600

    added notes on __init__() and __call__()

--- rough/restaurentclass_test.py
+++ rough/restaurentclass_test.py
@@ -13,3 +13,4 @@ print x.bankrupt
 x.open_branch()
 #self in a function is not a keyword, it is but a placeholder that holds the instance of the class that that function belongs to.
 #we can also use other varuavles in the place of self,but prefered not to. If a values is defined outside the __init__() finction it is called class variable.
+# __init__() and __call__(): the first builds an instance of Class up, the second makes such instance callable as a function would be without impacting the lifecycle of the object itself (i.e. __call__ does not impact the construction/destruction lifecycle) but it can modify its internal state (as shown below).
