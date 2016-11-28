ef313fa3073b97208792329feb3b71f89ce52db4
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-11-28 03:21:11 -0600

    Added ffot note for future reference , about overloading

--- args_kwargs.py
+++ args_kwargs.py
@@ -9,3 +9,5 @@ def soParameterize(**kwargs):
 icaniterateAnyThing(1,2,3,4,5,6,7,8,"mama")
 
 soParameterize(x=1,y=4,sting1="madhu")
+
+# python doesnot support overloading but using *args and **kwargs we can create branches inside defanitions./
