692a007fffb77bdecbaa4fdbef48b767c5faa9ca
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-12-28 16:04:52 -0600

    recurssion factorial

---
+++ factorial_recurssion.py
@@ -0,0 +1,10 @@
+# This is a example program that implements factorial operation using recursion.
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		return n*factorial(n-1)
+
+if __name__ =='__main__':
+	print factorial(5)
+	print factorial(6)
