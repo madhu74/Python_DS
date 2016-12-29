41cfeb1382dd63251a2c613f5d60a62f5b95943c
madhu74 <madhusudan.sridharan1990@gmail.com>
2016-12-28 22:49:09 -0600

    English ruler recursion.

---
+++ english_ruler.py
@@ -0,0 +1,22 @@
+# this is an exam for recursion, English ruler implementable.
+def draw_line(tick_length,tick_lable=''):
+	tick ="-"
+	line =tick*tick_length
+	if tick_lable:
+		line = line+' '+tick_lable
+	print line
+
+def draw_interval(centerlength):
+	if centerlength > 0 :
+		draw_interval(centerlength-1)
+		draw_line(centerlength)
+		draw_interval(centerlength-1)
+
+def draw_ruler(numb_inches,majorlength):
+	draw_line(majorlength,str(0))
+	for i in range(1,numb_inches+1):
+		draw_interval(majorlength)
+		draw_line(majorlength,str(i))
+
+if __name__ == '__main__':
+	draw_ruler(3,5)
